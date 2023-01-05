ecb7fbb8f32f2cacd4774a6a807bfbe3772e104f
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-01-05 18:51:45 +0100

    refactoring

--- reviews/views.py
+++ reviews/views.py
@@ -99,15 +99,15 @@ def create_review(request, ticket_id):
 @login_required
 def update_review(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
-    ticket = Ticket.objects.get(pk=review.ticket.pk)
     if request.user != review.user:
         raise PermissionDenied()
     review_form = UpdateReviewForm(instance=review)
     if request.method == "POST":
-        review_form = UpdateReviewForm(request.POST, request.FILES, instance=review)
+        review_form = UpdateReviewForm(request.POST, instance=review)
         if review_form.is_valid():
             review.save()
             return redirect("posts")
+    ticket = Ticket.objects.get(pk=review.ticket.pk)
     context = {"review_form": review_form, "ticket": ticket, "review": review}
     return render(request, "reviews/review_form.html", context)
 
@@ -145,14 +145,14 @@ def create_ticket_and_review(request):
 def subscriptions(request):
     form = SubscriptionForm()
     users = User.objects.all().order_by("username")
-    context = {"form": form, "users": users}
+    context = {"form": form, "users": users, "error_message": None}
     if request.method == "POST":
         form = SubscriptionForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data["username"]
             if username == request.user.username:
                 error_message = "Vous ne pouvez pas vous abonner à vous même."
-                context = {"form": form, "users": users, "error_message": error_message}
+                context["error_message"] = error_message
             else:
                 try:
                     subscription = Subscription()
@@ -161,10 +161,10 @@ def subscriptions(request):
                     subscription.save()
                 except User.DoesNotExist:
                     error_message = "Cet utilisateur n'existe pas."
-                    context = {"form": form, "users": users, "error_message": error_message}
+                    context["error_message"] = error_message
                 except IntegrityError:
                     error_message = "Vous êtes déjà abonné à cet utilisateur."
-                    context = {"form": form, "users": users, "error_message": error_message}
+                    context["error_message"] = error_message
     return render(request, "reviews/subscriptions.html", context)
 
 
