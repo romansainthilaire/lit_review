d58194a992772a6d7e5c293dfdf983f78cce4338
Roman Saint-Hilaire <sainthilaireroman@gmail.com>
2023-01-05 13:55:10 +0100

    customize admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from reviews.models import Ticket, Review, Subscription
 
-admin.site.register(Ticket)
-admin.site.register(Review)
+
+class TicketAdmin(admin.ModelAdmin):
+
+    list_display = ["title", "user", "time_created"]
+    list_filter = ["user"]
+
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_display = ["headline", "rating", "user", "time_created"]
+    list_filter = ["user"]
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Subscription)
